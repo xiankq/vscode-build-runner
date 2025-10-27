3277902a335c8074eb0f3937165100958806987f
xiankq <xiankq@qq.com>
2025-10-27 09:14:47 +0800

    feat(scanFile): 添加文件排除功能
    - 在 package.json 中新增 build_runner.excludes 配置项，用于指定扫描时排除的文件
    - 在 scanFile 函数中使用 @rollup/pluginutils 的 createFilter 方法实现文件过滤
    - 更新 pnpm-lock.yaml，添加新的依赖项 @rollup/pluginutils

--- src/shared/scanFile.ts
+++ src/shared/scanFile.ts
@@ -1,3 +1,4 @@
+import { createFilter } from "@rollup/pluginutils";
 import * as vsc from "vscode";
 
 export interface ScanFileTree {
@@ -10,14 +11,20 @@ export interface ScanFileTree {
  * @param pattern
  */
 export const scanFile = async (pattern: string): Promise<ScanFileTree[]> => {
+  const excludes: string[] | undefined = vsc.workspace
+    .getConfiguration()
+    .get("build_runner.excludes");
+  const filter = createFilter("**/**", excludes);
   const workspaces = vsc.workspace.workspaceFolders ?? [];
   const promises = workspaces.map(async (workspace) => {
     const relativePattern = new vsc.RelativePattern(workspace.uri, pattern);
     const fileUris = await vsc.workspace.findFiles(relativePattern);
+
     return {
       workspace,
-      fileUris,
+      fileUris: fileUris.filter(filter),
     };
   });
-  return Promise.all(promises);
+  const tree = await Promise.all(promises);
+  return tree.filter((e) => !!e.fileUris.length);
 };
